d828952375284e656576bb4864063d2f8945e899
Basel Anani <baselanani@Basels-Air.MG8702>
2022-02-17 20:30:16 -0700

    added some logging

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const auth = getAuth();
 async function signUpUser(email, password) {
     createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
         const user = userCredential.user;
-        console.log("Create Success@");
+        console.log("Create Success! " + user);
         window.location.href = "main.html"
     }).catch((error) => {
         const code = error.code
